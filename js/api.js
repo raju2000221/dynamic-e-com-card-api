3b2ef96bf1e37937263ef4c962d2bae3d6c3c7bc
raju2000221 <bdrajuislam246@gmail.com>
2023-03-28 21:27:21 +0600

    first saved

---
+++ js/api.js
@@ -0,0 +1,34 @@
+const loadPhones = async  (searchText) =>{
+    const url =`https://openapi.programming-hero.com/api/phones?search=${searchText} `
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data)
+    displayPhone(data.data)
+}
+const displayPhone = phones =>{
+        const phoneContainer = document.getElementById('phone_container');
+        phoneContainer.innerHTML ='';
+        phones.forEach( phone => {
+            const phoneDiv = document.createElement('div');
+       
+            phoneDiv.classList.add('col');
+            phoneDiv.innerHTML = `
+                <div class="card p-4">
+                <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text"> Brand: ${phone.brand}</p>
+                </div>
+                </div>
+            `;
+            phoneContainer.appendChild(phoneDiv);
+        });
+        
+}
+const searchText = text =>{
+    const searchfield =document.getElementById('search-field').value
+    loadPhones(searchfield)
+}
+
+
+loadPhones('iphone')
